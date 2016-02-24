b2213f44da9b2fa2184b1a7e0abf0ec3450552fe
Ivan Zlatev <ivan@ivanz.com>
2016-02-24 21:17:11 +0000

    Check-in the code.

---
+++ CorrelatorSharp.Logging/LogManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CorrelatorSharp.Logging
+{
+    public static class LogManager
+    {
+        public static ILogger GetCurrentClassLogger()
+        {
+            return LoggingConfiguration.LogManager.GetCurrentClassLogger();
+        }
+
+        public static ILogger GetLogger(string name)
+        {
+            return LoggingConfiguration.LogManager.GetLogger(name);
+        }
+
+    }
+}
